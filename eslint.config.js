5af67ec9e4ac52b53185bb603416dbbb55bff5ce
David Luna <luna.bistuer@gmail.com>
2025-02-15 01:09:03 +0100

    chore: more linting rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,29 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {languageOptions: {globals: globals.node}},
   pluginJs.configs.recommended,
   eslintConfigPrettier,
+  importPlugin.flatConfigs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    rules: {
+      'import/no-absolute-path': 'error',  
+      "import/no-duplicates": ["error", {"prefer-inline": true}],
+      'import/no-dynamic-require': 'error',
+      'import/no-named-default': 'error',
+      'import/no-namespace': 'error',
+      'import/no-webpack-loader-syntax': 'error',
+    },
+  }
 ];
+
+
